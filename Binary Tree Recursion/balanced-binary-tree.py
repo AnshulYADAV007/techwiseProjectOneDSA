b6b9f0118d7706df9d976d95045386832ec325eb
AnshulYADAV <anshulyadav98@gmail.com>
2022-11-09 07:13:39 +0530

    Added questions on binary tree

---
+++ Binary Tree Recursion/balanced-binary-tree.py
@@ -0,0 +1,24 @@
+# https://leetcode.com/problems/balanced-binary-tree/description/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        return self.getHeight(root) != -1
+
+    def getHeight(self, root):
+        if root == None:
+            return 0
+
+        left = self.getHeight(root.left)
+        right = self.getHeight(root.right)
+
+        if left == -1 or right == -1 or abs(left - right) > 1:
+            return -1
+
+        return max(left, right) + 1
